b2068b68833e1ac6ff0ebe2c15d647b344854b26
raytickb10 <raytickb10@students.ecu.edu>
2017-03-07 15:00:46 -0500

    Update pc3.c

--- pc3.c
+++ pc3.c
@@ -8,16 +8,31 @@ pthread_mutex_t the_mutex;      /* used for signaling */
 pthread_cond_t full, empty;    /* condition variables for the consumer and the producer */
 
 int buffer[BUFFER_SIZE];        /* buffer used between producer and consumer */
+int p_counter = 0;                     
+int c_counter = 0;                    
+
+void printBuffer()
+{
+    int i;
+    for (i = c_counter; i != p_counter; i = (i+1) % 10)
+    {
+        printf("%d ", buffer[i]);
+    }
+    printf("\n");
+    fflush(stdout);
+}
 
 void *producer(void *ptr)   /* produce data */
 {
-    int i;
+    int item, i;
 
     for (i = 1; i <= MAX; i++)
     {      
         pthread_mutex_lock(&the_mutex);   /* get exclusive access to buffer */
-        while (buffer[i] != 0) pthread_cond_wait(&empty, &the_mutex);
-        buffer[i] = i;                    /* put item in buffer */
+        buffer[p_counter] = item;
+        printf("Producer: insert item %d to %d\n", buffer[p_counter], in);
+        while ((p_counter + 1) % BUFFER_SIZE == c_counter) pthread_cond_wait(&empty, &the_mutex);  
+        p_counter = (p_counter + 1) % BUFFER_SIZE;                    /* put item in buffer */
         pthread_cond_signal(&full);      /* wake up consumer */
         pthread_mutex_unlock(&the_mutex); /* release access to buffer */
     }
@@ -26,13 +41,15 @@ void *producer(void *ptr)   /* produce data */
 
 void *consumer(void *ptr)   /* consume data */
 {
-    int i;
+    int item, i;
 
     for (i = 1; i <= MAX; i++)
     {      
         pthread_mutex_lock(&the_mutex);   /* get exclusive access to buffer */
-        while (buffer[i] == 0) pthread_cond_wait(&full, &the_mutex);
-        buffer[i] = 0;                    /* take item out of buffer */
+        while (p_counter == c_counter) pthread_cond_wait(&full, &the_mutex);    
+        item = buffer[c_counter];                     
+        printf("Consumer: consume item %d at %d\n", item, c_counter);
+        c_counter = (c_counter - 1) % BUFFER_SIZE;                    /* take item out of buffer */
         pthread_cond_signal(&empty);      /* wake up producer */
         pthread_mutex_unlock(&the_mutex); /* release access to buffer */
     }
