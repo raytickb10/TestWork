baf295fd82a0a0694c2d734effe7df586d480b1c
raytickb10 <raytickb10@students.ecu.edu>
2017-03-07 13:29:40 -0500

    Update pc2.c

--- pc2.c
+++ pc2.c
@@ -1,1 +1,14 @@
-asdnfsdfl
+int main(int argc, char **argv)
+{
+    pthread_t pro, con;
+    pthread_mutex_init(&the_mutex, 0);
+    pthread_cond_init(&condc, 0);
+    pthread_cond_init(&condp, 0);
+    pthread_create(&con, 0, consumer, 0);
+    pthread_create(&pro, 0, producer, 0);    
+    pthread_join(pro, 0);
+    pthread_join(con, 0);
+    pthread_cond_destroy(&condc);
+    pthread_cond_destroy(&condp);
+    pthread_mutex_destroy(&the_mutex);
+}
