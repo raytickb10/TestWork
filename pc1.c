fa2c6944398509cacd0129d723c880c919647ca4
raytickb10 <raytickb10@students.ecu.edu>
2017-03-07 14:32:56 -0500

    Update pc1.c

--- pc1.c
+++ pc1.c
@@ -4,8 +4,6 @@
 #define MAX 1000000             /* how many numbers to produce */
 #define BUFFER_SIZE 10          /* buffer size */
 
-pthread_cond_t full, empty;    /* condition variables for the consumer and the producer */
-
 int buffer[BUFFER_SIZE];        /* buffer used between producer and consumer */
 
 void *producer(void *ptr)   /* produce data */
@@ -14,9 +12,7 @@ void *producer(void *ptr)   /* produce data */
 
     for (i = 1; i <= MAX; i++)
     {      
-        while (buffer[i] != 0) pthread_cond_wait(&empty);
         buffer[i] = i;                    /* put item in buffer */
-        pthread_cond_signal(&full);      /* wake up consumer */
     }
     pthread_exit(0);
 }
@@ -27,9 +23,7 @@ void *consumer(void *ptr)   /* consume data */
 
     for (i = 1; i <= MAX; i++)
     {      
-        while (buffer[i] == 0) pthread_cond_wait(&full);
         buffer[i] = 0;                    /* take item out of buffer */
-        pthread_cond_signal(&empty);      /* wake up producer */
     }
     pthread_exit(0);
 }
@@ -37,12 +31,8 @@ void *consumer(void *ptr)   /* consume data */
 int main(int argc, char **argv)
 {
     pthread_t pro, con;
-    pthread_cond_init(&full, 0);
-    pthread_cond_init(&empty, 0);
     pthread_create(&con, 0, consumer, 0);
     pthread_create(&pro, 0, producer, 0);    
     pthread_join(pro, 0);
     pthread_join(con, 0);
-    pthread_cond_destroy(&full);
-    pthread_cond_destroy(&empty);
 }
